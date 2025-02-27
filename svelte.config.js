fbd48ae6d54fa1f55c8898dde8c67c156eb7249e
tolstovrob <tolstovrob@gmail.com>
2025-02-27 18:48:13 +0400

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,28 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			'$shared/components': './src/shared/components',
+			'$shared/utils': './src/shared/utils',
+			$shared: './src/shared',
+			$features: './src/features',
+			$services: './src/services',
+			$pages: './src/pages',
+			$routes: './src/routes',
+			$app: './src/app',
+		},
+	},
+};
+
+export default config;
