75e52e36e6b8882bb04cf2f6a4f6ee76fc7b5463
tolstovrob <tolstovrob@gmail.com>
2025-03-30 02:32:09 +0400

    fix(api): update MutationResponse

--- src/shared/api/query.ts
+++ src/shared/api/query.ts
@@ -1,6 +1,6 @@
 import {
 	QueryClient,
-	type MutationObserverResult,
+	type CreateMutationResult,
 	type QueryObserverResult,
 } from '@tanstack/svelte-query';
 import type { Readable } from 'svelte/store';
@@ -15,19 +15,19 @@ export const queryClient = new QueryClient({
 });
 
 /**
- * @param T data you receive at the end
+ * @template T data you receive at the end
  */
 export type QueryResponse<T> = Readable<QueryObserverResult<T, Error>>;
 
 /**
- * @param TData data you receive at the end
- * @param TVariables mutation arguments. E.x. something you send to server
- * @param TContext abort context. Contains previous data
+ * @template TRequest data you send to server
+ * @template TResponse data you get back from server
  * @example MutationResponse<EntityResponse> // simple usage
  * @example MutationResponse<EntityResponse, { id: number }, { prev: EntityResponse }> // with
  */
-export type MutationResponse<TData, TVariables = void, TContext = unknown> = {
-	mutate: (variables: TVariables) => void;
-	mutateAsync: (variables: TVariables) => Promise<TData>;
-	reset: () => void;
-} & Readable<MutationObserverResult<TData, Error, TVariables, TContext>>;
+export type MutationResponse<TRequest, TResponse> = CreateMutationResult<
+	TResponse,
+	Error,
+	TRequest,
+	unknown
+>;
