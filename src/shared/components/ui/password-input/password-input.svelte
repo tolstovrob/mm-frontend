fd0dd15f842c7b0c2bb89a12157409e8bdb4568c
tolstovrob <tolstovrob@gmail.com>
2025-03-30 01:16:44 +0400

    feat(ui): add PasswordInput

---
+++ src/shared/components/ui/password-input/password-input.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Input } from '$shared/components/ui/input';
+	import type { WithElementRef } from 'bits-ui';
+	import { Eye, EyeOff } from 'lucide-svelte';
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	type Props = WithElementRef<HTMLInputAttributes> & {
+		showPassword: boolean;
+		toggleShowPassword: (event: MouseEvent) => void;
+		value: string;
+	};
+
+	let { showPassword, toggleShowPassword, value = $bindable(), ...restProps }: Props = $props();
+</script>
+
+<div class="relative">
+	<Input
+		{...restProps}
+		bind:value
+		type={showPassword ? 'text' : 'password'} />
+	<button
+		class="absolute right-3 top-3"
+		onclick={toggleShowPassword}>
+		{#if showPassword}
+			<Eye size="16" />
+		{:else}
+			<EyeOff size="16" />
+		{/if}
+	</button>
+</div>
