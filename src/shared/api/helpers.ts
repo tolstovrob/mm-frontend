86cff90a8dbf3aa56f94c9fca68894610e6852be
tolstovrob <tolstovrob@gmail.com>
2025-03-30 21:33:17 +0400

    feat(api): add server static files handler

--- src/shared/api/helpers.ts
+++ src/shared/api/helpers.ts
@@ -7,3 +7,12 @@
  */
 export const backend = (endpoint: string): string =>
 	`${import.meta.env.VITE_BACKEND_PROTOCOL}://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}${endpoint}`;
+
+/**
+ * Creates and returns a full server route to a static file
+ *
+ * @param filename full path of the file in /static. E.x. aboba/index.webp will return smth like this: http://localhost:3000/static/aboba/index.webp
+ * @returns complete route on server
+ */
+export const staticfile = (filename: string): string =>
+	`${import.meta.env.VITE_BACKEND_PROTOCOL}://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/static/${filename}`;
