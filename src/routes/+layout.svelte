d2176c6bff032cbd1afc5a91b5d88157765baa59
tolstovrob <tolstovrob@gmail.com>
2025-03-30 12:29:09 +0400

    feat(ui): add LightSwitch via mode-watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import { queryClient } from '$shared/api';
 	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <QueryClientProvider client={queryClient}>
 	{@render children()}
 </QueryClientProvider>
