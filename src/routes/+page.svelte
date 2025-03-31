4b1a1f7f45a432adf1c3e5a26276f9b30e04aedc
tolstovrob <tolstovrob@gmail.com>
2025-03-31 13:04:30 +0400

    fix(api): remove bad fetching && set typescript as script language

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,8 @@
-<script>
-	import * as Example from '$features/Example';
+<script lang="ts">
 	import { Button } from '$shared/components/ui/button';
-
-	const sender = Example.checkHealth();
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <Button>Aboba</Button>
-
-<pre>
-	{JSON.stringify($sender.isPending)}
-	{JSON.stringify($sender.error) ?? 'no error'}
-	{JSON.stringify($sender.data) ?? 'no data'}
-</pre>
