ca3c61b27a6e22392f36c8f8b287e44c60b79e26
tolstovrob <tolstovrob@gmail.com>
2025-03-31 14:02:55 +0400

    feat(ui): add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Navbar } from '$widgets/Navbar';
+	import { Footer } from '$widgets/Footer';
 	import '../app.css';
 
 	let { children } = $props();
@@ -11,7 +12,8 @@
 <ModeWatcher />
 <QueryClientProvider client={queryClient}>
 	<Navbar />
-	<main class="container py-8 border-x-[1px] border-border">
+	<main class="container py-8 border-x-[1px] border-border h-full">
 		{@render children()}
 	</main>
+	<Footer />
 </QueryClientProvider>
---
+++ src/widgets/Footer/footer.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { GitPullRequest } from 'lucide-svelte';
+	import * as User from '$features/User';
+
+	const session = User.session();
+	let stringifiedSession = $derived(
+		$session.isPending
+			? 'загрузка...'
+			: $session.data && 'firstName' in $session.data
+				? $session.data.firstName + ' ' + $session.data.lastName
+				: 'отсутствует',
+	);
+</script>
+
+<footer class="w-full border-y-[1px] border-border">
+	<div
+		class="container flex flex-row justify-between items-center border-x-[1px] py-6 border-border">
+		<div class="flex flex-col gap-4">
+			<a
+				title="Brand"
+				href="/"
+				class="flex items-center gap-2 font-bold select-none text-xl">
+				<div
+					class="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+					<GitPullRequest class="size-4" />
+				</div>
+				MyBrand
+			</a>
+			<p class="text-muted-foreground">
+				MyBrand {new Date(Date.now()).getFullYear()}. Все права защищены
+			</p>
+		</div>
+
+		Текущая сессия: {stringifiedSession}
+	</div>
+</footer>
