d0b59bbcd1bf39561860e98a5ef0849ed93cb60a
tolstovrob <tolstovrob@gmail.com>
2025-04-01 08:37:46 +0400

    feat(ui): add filter reset button

--- src/shared/components/ui/search-bar/search-bar.svelte
+++ src/shared/components/ui/search-bar/search-bar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Button } from '$shared/components/ui/button';
 	import { Input } from '$shared/components/ui/input';
-	import { Search } from 'lucide-svelte';
+	import { Search, X } from 'lucide-svelte';
 
 	type Props = {
 		filter: string;
@@ -13,12 +13,22 @@
 
 <form
 	onsubmit={handleSubmit}
-	class="flex flex-row items-center gap-1">
+	class="relative flex flex-row items-center gap-1">
 	<Input
 		type="text"
 		placeholder="Найти нужный курс..."
 		class="h-12"
 		bind:value={filter} />
+	<Button
+		onclick={() => {
+			filter = '';
+			handleSubmit();
+		}}
+		class={`absolute right-14 ${filter === '' && 'hidden'}`}
+		size="icon"
+		variant="ghost">
+		<X />
+	</Button>
 	<Button
 		type="submit"
 		variant="secondary"
