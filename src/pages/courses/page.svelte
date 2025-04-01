ad2f7c0789d8b03597f8599814a8d0286ff49cc6
tolstovrob <tolstovrob@gmail.com>
2025-04-01 15:21:48 +0400

    refactor(ui): move CourseCard to shared/components/ui

--- src/pages/courses/page.svelte
+++ src/pages/courses/page.svelte
@@ -3,11 +3,11 @@
 	import { Button } from '$shared/components/ui/button';
 	import { SearchBar } from '$shared/components/ui/search-bar';
 	import { Skeleton } from '$shared/components/ui/skeleton';
-	import { CourseCard } from '$widgets/CourseCard';
+	import { CourseCard } from '$shared/components/ui/course-card';
 	import { RotateCcw } from 'lucide-svelte';
 
 	let filter: string = $state('');
-	const coursesQuery = $derived(Course.fetchCourses(filter));
+	const coursesQueue = $derived(Course.fetchCourses(filter));
 </script>
 
 <header class="mt-6 grid grid-cols-1 grid-rows-2 gap-8 lg:grid-cols-2 lg:grid-rows-1">
@@ -15,32 +15,32 @@
 	<SearchBar
 		bind:filter
 		handleSubmit={() => {
-			$coursesQuery.refetch();
+			$coursesQueue.refetch();
 		}} />
 </header>
 
 <div class="my-12 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-	{#if $coursesQuery.isPending}
+	{#if $coursesQueue.isPending}
 		<Skeleton class="min-h-40" />
 		<Skeleton class="min-h-40" />
 		<Skeleton class="min-h-40" />
 		<Skeleton class="min-h-40" />
 		<Skeleton class="min-h-40" />
 		<Skeleton class="min-h-40" />
-	{:else if $coursesQuery.error}
+	{:else if $coursesQueue.error}
 		<div class="col-span-1 flex flex-row items-center gap-4 md:col-span-2 lg:col-span-3">
 			<span class="text-lg text-destructive">
-				Не удалось загрузить курсы: {$coursesQuery.error}
+				Не удалось загрузить курсы: {$coursesQueue.error}
 			</span>
 			<Button
 				size="icon"
 				variant="ghost"
-				onclick={() => $coursesQuery.refetch()}>
+				onclick={() => $coursesQueue.refetch()}>
 				<RotateCcw />
 			</Button>
 		</div>
 	{:else}
-		{#each $coursesQuery.data as course}
+		{#each $coursesQueue.data as course}
 			<CourseCard {course} />
 		{:else}
 			<div>
@@ -49,7 +49,7 @@
 					class="ml-2"
 					size="icon"
 					variant="ghost"
-					onclick={() => $coursesQuery.refetch()}>
+					onclick={() => $coursesQueue.refetch()}>
 					<RotateCcw />
 				</Button>
 			</div>
---
+++ src/shared/components/ui/course-card/course-card.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import type { ICourseCredentials } from '$features/Course/types';
+	import * as Card from '$shared/components/ui/card';
+	import * as AvatarGroup from '$shared/components/ui/avatar-group';
+	import * as icons from 'lucide-svelte';
+	import type { SvelteComponent } from 'svelte';
+	import { UserCard } from '$shared/components/ui/user-card';
+
+	type Props = { course: ICourseCredentials };
+
+	let { course }: Props = $props();
+	const Icon = (
+		course.icon in icons ? icons[course.icon as keyof typeof icons] : icons.Book
+	) as typeof SvelteComponent<{
+		size?: number;
+		color?: string;
+		class?: string;
+	}>;
+</script>
+
+<a href={`/courses/${course.id}`}>
+	<Card.Root
+		class="group relative flex min-h-40 flex-col justify-between overflow-hidden transition-all hover:bg-accent">
+		<div class="absolute -right-10 z-0 opacity-5 transition-all group-hover:-rotate-[15deg]">
+			<Icon size={160} />
+		</div>
+		<Card.Header>
+			<Card.Title class="line-clamp-2 text-2xl leading-tight">
+				<span class="max-w-[75%]">{course.title}</span>
+			</Card.Title>
+		</Card.Header>
+		<Card.Footer class="flex flex-wrap gap-2">
+			<AvatarGroup.Root>
+				{#each course.teachers.slice(0, 3) as teacher}
+					<UserCard user={teacher} />
+				{/each}
+				{#if course.teachers.length - 3 > 0}
+					<AvatarGroup.Etc plus={course.teachers.length - 3} />
+				{/if}
+			</AvatarGroup.Root>
+		</Card.Footer>
+	</Card.Root>
+</a>
