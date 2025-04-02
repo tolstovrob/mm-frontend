7172b5cfd7c3350e8e7a26afc2b4e8832ab869b2
tolstovrob <tolstovrob@gmail.com>
2025-04-02 15:21:43 +0400

    feat(ui): add course page

--- src/pages/courses/id/page.svelte
+++ src/pages/courses/id/page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import * as Card from '$shared/components/ui/card';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
 	import * as Course from '$features/Course';
-	import { UserCard } from '$shared/components/ui/user-card';
 	import * as User from '$features/User';
+	import * as Card from '$shared/components/ui/card';
+	import { UserCard } from '$shared/components/ui/user-card';
 	import { Skeleton } from '$shared/components/ui/skeleton';
 	import { Button } from '$shared/components/ui/button';
-	import { RotateCcw } from 'lucide-svelte';
 	import * as icons from 'lucide-svelte';
 	import type { SvelteComponent } from 'svelte';
 
@@ -46,7 +45,7 @@
 			size="icon"
 			variant="ghost"
 			onclick={() => $courseQuery.refetch()}>
-			<RotateCcw />
+			<icons.RotateCcw />
 		</Button>
 	</div>
 {:else if $courseQuery.data}
@@ -79,4 +78,58 @@
 			</div>
 		</Card.Content>
 	</Card.Root>
+	{#each $courseQuery.data.groups as courseGroup (courseGroup.id)}
+		<Card.Root
+			class={`group relative mt-8 ${!courseGroup.isAllowed && 'cursor-not-allowed select-none'}`}>
+			<Card.Header class={`${!courseGroup.isAllowed && 'blur-sm'}`}>
+				<Card.Title>{courseGroup.title}</Card.Title>
+			</Card.Header>
+			<Card.Content class={`${!courseGroup.isAllowed && 'blur-sm'}`}>
+				<ul class="flex flex-col gap-4">
+					{#snippet item(courseId: number, courseItem: Course.ICourseItemCredentials)}
+						<a
+							href={`/courses/${courseId}/${courseItem.id}`}
+							class="flex max-w-max items-center gap-4 underline-offset-4 hover:underline">
+							{#if courseItem.type === 'info'}
+								<icons.Clipboard
+									size={20}
+									class="text-muted-foreground" />
+							{:else if courseItem.type === 'select'}
+								<icons.CircleCheckBig
+									size={20}
+									class="text-muted-foreground" />
+							{:else if courseItem.type === 'quiz'}
+								<icons.ListTodo
+									size={20}
+									class="text-muted-foreground" />
+							{:else if courseItem.type === 'upload'}
+								<icons.Upload
+									size={20}
+									class="text-muted-foreground" />
+							{:else if courseItem.type === 'file'}
+								<icons.Paperclip
+									size={20}
+									class="text-muted-foreground" />
+							{/if}
+							{#if ['upload', 'quiz'].includes(courseItem.type)}
+								<b>{courseItem.title}</b>
+							{:else}
+								{courseItem.title}
+							{/if}
+						</a>
+					{/snippet}
+					{#each courseGroup.items as courseItem (courseItem.id)}
+						{@render item($courseQuery.data.id, courseItem)}
+					{/each}
+				</ul>
+			</Card.Content>
+			<div
+				class={`absolute top-0 z-10 h-full w-full flex-col items-center justify-center gap-2 rounded-lg bg-background bg-opacity-50 ${courseGroup.isAllowed ? 'hidden' : 'flex'}`}>
+				<icons.LockKeyhole size={64} />
+				<div class="max-w-96 text-center">
+					Данный раздел пока что не доступен, обратитесь к преподавателю
+				</div>
+			</div>
+		</Card.Root>
+	{/each}
 {/if}
